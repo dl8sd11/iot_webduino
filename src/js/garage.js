99f999eca7ed9085f846b5dd9bea2abcda4a5579
dl8sd11 <j312094@nehs.hc.edu.tw>
2018-01-22 21:16:58 +0800

    traffic javascript

---
+++ src/js/garage.js
@@ -0,0 +1,20 @@
+// function makeGarage(callback) {
+//
+//   callback();
+// }
+var garageID = '10Q84jXQ';
+var ButtonPort = 4;
+boardReady({board: 'Smart', device: garageID, transport: 'mqtt'}, function (board) {
+  board.systemReset();
+  board.samplingInterval = 50;
+  console.log("garage connected");
+  button = getButton(board, ButtonPort);
+  button.on("released", function(){
+    console.log("Pressed");
+    $('#garage').show();
+  });
+  button.on("pressed",function(){
+    $('#garage').hide();
+    console.log("released");
+  });
+});
